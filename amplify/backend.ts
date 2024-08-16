8ef81f250f7a0156c30bef6d0d3a6ad9904c57d9
incremental capital <incrementalcapital@gmail.com>
2024-08-16 10:49:30 +0900

    fixed function names

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,7 +1,7 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
-import { function as groqQueryFunction } from './functions/groqQuery/resource.js';
+import { groqQueryFunction } from './functions/groqQuery/resource.js';
 import { api } from './api/resource.js';
 
 export const backend = defineBackend({
--- amplify/functions/groqQuery/resource.ts
+++ amplify/functions/groqQuery/resource.ts
@@ -1,7 +1,7 @@
 import { defineFunction, Permissions } from '@aws-amplify/backend';
 import { Function } from '@aws-amplify/backend-cli';
 
-export const function = defineFunction({
+export const groqQueryFunction = defineFunction({
   name: 'groqQuery',
   handler: 'index.handler',
   runtime: Function.Runtime.NODEJS_18,
